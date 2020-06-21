75cedf65900da5573f996bedd6a1dccedb8ef7c3
Muhammad Mubashir <66302833+MubashirAslam78@users.noreply.github.com>
2020-06-21 19:05:19 +0500

    updated commit

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <h1 clss="title" >
+            <strong class="exp" >E</strong>xpense <strong class="tra" >T</strong>racker
+        </h1>
+    )
+}
